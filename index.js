e3a36e59fb0d92d1baf209f98ad5894d243464b5
Bartlomiej_Stawiarz <bartlomiej.stawiarz@crimsonpine.com>
2022-10-31 15:11:36 +0100

    Attempt to retrieve more info

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+console.log('--- RUNNING AUTO CHANGELOG ---')
 
 try {
     console.log(JSON.stringify(github.event.pull_request));
+    console.log('Tried');
 }
 catch (error) {
     core.setFailed(error.message);
+    console.log('Failed');
 }
