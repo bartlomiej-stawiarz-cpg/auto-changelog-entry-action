85fab879a46eaf43c16e5bebca72caa117378393
Bartlomiej_Stawiarz <bartlomiej.stawiarz@crimsonpine.com>
2022-10-31 14:39:29 +0100

    Attempt to fix undefined

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import core from '@actions/core'
-import github from '@actions/github'
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 
 try {
-    console.log(JSON.stringify(github.context.payload, undefined, 2))
+    console.log(JSON.stringify(github.context.payload, undefined, 2));
 }
 catch (error) {
     core.setFailed(error.message);
