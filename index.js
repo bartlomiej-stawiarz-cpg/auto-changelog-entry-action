92513a1618a64bd0e0b08c12b244ef1bf88a0784
Bartlomiej_Stawiarz <bartlomiej.stawiarz@crimsonpine.com>
2022-10-31 15:16:54 +0100

    Removed pr info

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-console.log('--- RUNNING AUTO CHANGELOG ---')
+console.log('--- RUNNING AUTO CHANGELOG ---');
 
 try {
-    console.log(JSON.stringify(github.event.pull_request));
     console.log('Tried');
 }
 catch (error) {
