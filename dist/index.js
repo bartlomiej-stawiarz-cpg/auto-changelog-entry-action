647e22a34f9ca1e52d59b4a42091f3aaf03c667c
Bartlomiej_Stawiarz <bartlomiej.stawiarz@crimsonpine.com>
2022-11-03 22:47:59 +0100

    Missing return value build

--- dist/index.js
+++ dist/index.js
@@ -9751,6 +9751,8 @@ function prepareChangelogEntryText(template, templateVariableDefinitions) {
     Object.keys(templateVariableDefinitions).forEach(key => {
         template = template.replaceAll(`$${key.toUpperCase().trim()}`, templateVariableDefinitions[key]);
     });
+
+    return template;
 }
 
 
